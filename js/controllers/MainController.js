097ac7d562745510e44fbf30b4045201a2431d65
Diana Monteiro <dianamonteiro@MacBook-Pro-de-Diana.local>
2016-05-23 22:28:28 +0100

    fucking scroll works

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,59 +1,46 @@
 app.controller("MainController", function($scope, $http) {
 
-  // the array which represents the list
-  //$scope.listOfEntries = data.hits;
-    $scope.loading = true;
-    $scope.list    = [];
+    $scope.url            =  'http://hn.algolia.com/api/v1/search_by_date?tags=story&hitsPerPage=10';
+    var hitsN             = 10;
+    $scope.loading        = true;
+    $scope.listOfEntries  = [];
 
-  // this function fetches the api and adds it to array
     $scope.more = function() {
 
-        var url =  'http://hn.algolia.com/api/v1/search_by_date?tags=story&hitsPerPage=5';
+        $scope.url   =  'http://hn.algolia.com/api/v1/search_by_date?tags=story&hitsPerPage=' + hitsN;
 
-
-        $http.get(url)
+        $http.get($scope.url)
             .success(function(data) {
               if( ! data ) return;
-
-               var newItem;
+              var items = data.hits;
               
-              $scope.listOfEntries = [];
-
-              for ( line in data.hits ) {
-
-                newItem = ( data.hits.length + 1 );
-
-                $scope.listOfEntries.push( data.hits[ line ] );
-
+              for (var i = 0; i < items.length; i++) {
+               
+               $scope.listOfEntries.push(items[i]);
               }
+              console.log($scope.listOfEntries);
+              hitsN = hitsN+1;
 
               $scope.loading = false;
 
             });
     };
-  // we call the function twice to populate the list
+
   $scope.more();
+
 });
 
-// we create a simple directive to modify behavior of <ul>
-app.directive("whenScrolled", function() {
-  return {
+app.directive("whenScrolled", function($window){
+  return function(scope, element, attrs) {
 
-    restrict: 'A',
-    link: function(scope, elem, attrs) {
+      raw = element[0];
 
-      // we get a list of elements of size 1 and need the first element
-      raw = elem[0];
-
-      // we load more elements when scrolled past a limit
-      elem.bind("scroll", function() {
-        if (raw.scrollTop + raw.offsetHeight + 5 >= raw.scrollHeight) {
+      angular.element($window).bind("scroll", function() {
+        if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
+            alert("scroll");
           scope.loading = true;
-
-          // we can give any function which loads more elements into the list
-          scope.$apply(attrs.whenScrolled);
         }
+        scope.$apply(attrs.whenScrolled);
       });
     }
-  }
 });
